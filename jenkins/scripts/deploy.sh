da56899da51c69499be7aa02effd460a7b83aaa6
BartoszWolodzko <74002652+BartoszWolodzko@users.noreply.github.com>
2022-11-02 15:08:47 +0100

    Update deploy.sh

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -11,6 +11,13 @@ docker network create test-net
 docker container run -d \
     --name api \
     --net test-net \
+    -p 3000:3000 \
+    $registry:$BUILD_NUMBER
+
+docker container run -d \
+    --name api \
+    --net test-net \
+    -p 3000:3000 \
     $registry:$BUILD_NUMBER
 
 # Logic to wait for the api component to be ready on port 3000
